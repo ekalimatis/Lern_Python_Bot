91fccb31cd58b4f134afb7ce3dd2afd3a0e7fce8
EKA <eka@limantis.com>
2022-09-04 11:33:41 +0300

    echo bot

---
+++ bot.py
@@ -0,0 +1,26 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from settings import API_KEY
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+def greet_user(update, context):
+    print('Start', update, context)
+    update.message.reply_text('Привет')
+
+def answer_me(update, context):
+    update.message.reply_text(update.message.text)
+
+def main():
+    mybot = Updater(API_KEY, use_context=True)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(MessageHandler(Filters.text, answer_me))
+
+    logging.info('Start')
+    mybot.start_polling()
+    mybot.idle()
+
+if __name__ == '__main__':
+    main()
