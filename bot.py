f35034a89c6b5950aeae9230563c36b4bfdd6a22
EKA <eka@limantis.com>
2022-09-05 08:50:28 +0300

    fixed

--- bot.py
+++ bot.py
@@ -1,18 +1,19 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 from settings import API_KEY
 
-logging.basicConfig(filename='bot.log', level=logging.INFO)
+logging.basicConfig(filename = 'bot.log', level = logging.INFO)
 
 def greet_user(update, context):
-    print('Start', update, context)
+    logging.info('Start', update, context)
     update.message.reply_text('Привет')
 
 def answer_me(update, context):
     update.message.reply_text(update.message.text)
 
 def main():
-    mybot = Updater(API_KEY, use_context=True)
+    mybot = Updater(API_KEY, use_context = True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
